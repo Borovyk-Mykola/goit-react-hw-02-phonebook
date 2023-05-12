9d876104ab23b0edb908ea4409057874f070a581
Borovyk-Mykola <114910601+Borovyk-Mykola@users.noreply.github.com>
2023-05-12 19:53:51 +0300

    finish2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,13 @@ export const App = () => {
     contacts: [],
     filter: '',
   }
+
+  addContact = () => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts + 1,        
+    }));
+  };
+
   return (
     <div
       style={{
@@ -14,7 +21,19 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <h1>Phonebook</h1>
+      <div>
+        <input 
+        type="text" 
+        name="name" 
+        pattern="^[a-zA-Za-яА-Я]+(([' -][a-zA-Za-яА-Я ])?[a-zA-Za-яА-Я]*)*$" 
+        title="Name may contain onle letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Bats de Castelmore d`Artagnan" 
+        required/>
+        <button type="submit" onSubmit={this.addContact}>Add contact</button>
+      </div>
+      <h2>Contacts</h2>
+      <div></div>
+      <div></div>
     </div>
   );
 };
