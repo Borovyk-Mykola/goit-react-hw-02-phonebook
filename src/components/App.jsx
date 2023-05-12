d13ba2fd3cb6cbcefe3aebab48822dc7d02d0404
Borovyk-Mykola <114910601+Borovyk-Mykola@users.noreply.github.com>
2023-05-13 01:59:12 +0300

    45

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,59 @@
-export const App = () => {
-  state = {
-    contacts: [],
-    filter: '',
-  }
+import React from 'react';
+import ContactList from './ContackList/ContactList'
+import { nanoid } from 'nanoid';
 
-  addContact = () => {
+const CONTACT_KEY = 'Add contact'
+
+export class App extends React.Component {
+  state = {contacts: [], filter: ''}
+
+  deleteContact = (contactId) => {
     this.setState(prevState => ({
-      contacts: prevState.contacts + 1,        
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
 
+  handleOnChange = e => {
+    this.setState({contacts: [{name: `${e.currentTarget.value}`, id: `${nanoid()}`}]});
+    if(JSON.parse(localStorage.getItem(CONTACT_KEY)) !== null) {
+      this.setState(prevState =>
+      localStorage.setItem(CONTACT_KEY, JSON.stringify((prevState.contacts).push(JSON.parse(localStorage.getItem(CONTACT_KEY))))))
+    } else {return}
+  };
+
+  addContact = e => {
+    e.preventDefault();
+    localStorage.setItem(CONTACT_KEY, JSON.stringify(this.state.contacts));
+  };
+  
+  render() {
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
+        textAlign: 'center',
+        fontSize: 28,
         color: '#010101'
       }}
     >
       <h1>Phonebook</h1>
-      <div>
+      <form onSubmit={this.addContact}>
         <input 
-        type="text" 
+        type="text"
         name="name" 
+        value={this.state.contacts.name}
         pattern="^[a-zA-Za-яА-Я]+(([' -][a-zA-Za-яА-Я ])?[a-zA-Za-яА-Я]*)*$" 
         title="Name may contain onle letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Bats de Castelmore d`Artagnan" 
-        required/>
-        <button type="submit" onSubmit={this.addContact}>Add contact</button>
-      </div>
+        required
+        onChange={this.handleOnChange}
+        />
+        <button type="submit" onClick={this.addNewContact}>Add contact</button>
+      </form>
       <h2>Contacts</h2>
-      <div></div>
+      {JSON.parse(localStorage.getItem(CONTACT_KEY)) !== '' ?
+        <ContactList contacts={this.state.contacts} onDeleteContact={this.deleteContact}></ContactList>
+      : ''}
       <div></div>
     </div>
-  );
+  )}
 };
